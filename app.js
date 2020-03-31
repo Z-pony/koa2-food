398063ba8d7eb06f09f3168e8d1ba8ec142795fa
zhangyafang <877473043@qq.com>
2020-03-31 13:16:47 +0800

    eslint配置

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
-const Koa = require('koa')
-const app = new Koa()
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const session = require('koa-generic-session')
-const redisStore = require('koa-redis')
-const {REDIS_CONF} = require('./conf/db')
+const Koa = require('koa'),
+    app = new Koa();
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+const { REDIS_CONF } = require('./conf/db');
 
-const users = require('./routes/user')
+const users = require('./routes/user');
 
 // error handler
-onerror(app)
-
+onerror(app);
 // middlewares
 app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
+    'enableTypes': [ 'json', 'form', 'text' ]
+}));
+app.use(json());
+app.use(logger());
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
 
-// session配置
-app.keys = ['WJol#23123_']
+    const start = new Date();
+
+    await next();
+    const ms = new Date() - start;
+
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+
+});
+
+// session配置
+app.keys = [ 'WJol#23123_' ];
 app.use(session({
-  //配置cookie
-  cookie: {
-    path: '/',
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000
-  },
+    // 配置cookie
+    'cookie': {
+        'path': '/',
+        'httpOnly': true,
+        'maxAge': 24 * 60 * 60 * 1000
+    },
 
-  // 配置redis
-  store: redisStore({
+    // 配置redis
+    'store': redisStore({
     // all: '127.0.0.1:6379', //写死本地的redis
-    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
-  })
-}))
+        'all': `${REDIS_CONF.host}:${REDIS_CONF.port}`
+    })
+}));
 
 // routes
 // app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+
+    console.error('server error', err, ctx);
+
 });
 
-module.exports = app
+module.exports = app;
